f83364c8089608e44cce9867c690f7e20b85f568
Ilya-Kolomin <ilya-kolomin2001@yandex.ru>
2021-08-18 11:39:00 +0300

    Initial commit2

---
+++ cmake-build-debug/CMakeFiles/ReversedCodeOfString.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReversedCodeOfString.dir/main.cpp.o"
+  "ReversedCodeOfString"
+  "ReversedCodeOfString.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReversedCodeOfString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
