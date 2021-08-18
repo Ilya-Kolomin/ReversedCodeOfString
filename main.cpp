a39be3f23d4b33a740c64f4fa5eb72f684f1798c
Ilya-Kolomin <ilya-kolomin2001@yandex.ru>
2021-08-18 11:44:34 +0300

    Finished project

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-
+    string s;
+    cout << "Enter string:" << endl;
+    cin >> s;
+    for (int i = s.size() - 1; i >= 0; i--) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            cout << (int)(s[i] - 'A' + 1) << " ";
+        } else {
+            cout << (int)(s[i] - 'a' + 1) << " ";
+        }
+    }
+    cout << endl;
     return 0;
 }
